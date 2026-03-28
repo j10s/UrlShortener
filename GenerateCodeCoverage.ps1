5ee47e667f8b5408608c52a8233b50a4db5e49ab
Jamie Stevens <j10s@users.noreply.github.com>
2026-03-28 13:54:08 +0000

    Fix local code coverage

--- GenerateCodeCoverage.ps1
+++ GenerateCodeCoverage.ps1
@@ -1,9 +1,9 @@
-if (Test-Path .\UrlShortener.IntegrationTests\TestResults) {
-    Remove-Item -Recurse -Force .\UrlShortener.IntegrationTests\TestResults
+if (Test-Path UrlShortener.IntegrationTests\TestResults) {
+    Remove-Item -Recurse -Force UrlShortener.IntegrationTests\TestResults
 }
-if (Test-Path .\UrlShortener.IntegrationTests\TestResults) {
-    Remove-Item -Recurse -Force .\UrlShortener.UnitTests\TestResults
+if (Test-Path UrlShortener.UnitTests\TestResults) {
+    Remove-Item -Recurse -Force UrlShortener.UnitTests\TestResults\
 }
 
 dotnet test UrlShortener.sln --collect:"XPlat Code Coverage" --settings:coverage.runsettings
-reportgenerator -reports:./**/coverage.cobertura.xml -targetdir:./CoverageReport -reporttypes:Html
+reportgenerator -reports:./**/coverage.cobertura.xml -targetdir:./CoverageReport -reporttypes:HtmlInline
